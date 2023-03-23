4cafacc408b0183d72dfd159b8a642e8c0b2874a
xiaochengxin <2456841957@qq.com>
2023-03-23 11:05:07 +0800

    3.23

---
+++ test_3_22(1)/test1.cpp
@@ -0,0 +1,69 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	int count = 0;
+//	for (i = 1;i <= 100;i++) {
+//		if (i % 10 == 9) {
+//			count++;
+//		}
+//		if (i / 10 == 9) {
+//			count++;
+//		}
+//	}
+//	printf("count=%d\n", count);
+//	return 0;
+//}
+//#include<stdio.h>
+//
+//int main()
+//{
+//	int i = 0;
+//	int flag = 1;
+//	double sum = 0.0;
+//	double sz = 0.0;
+//	for (i = 1;i <= 100;i++) {
+//		sz = 1.0 / i;
+//		sum = sum + sz * flag;
+//		flag = -flag;
+//	}
+//	printf("sum=%lf\n", sum);
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,18,8,9,10 };
+//	int len = sizeof(arr) / sizeof(arr[0]);
+//	int n = 0;
+//	int max = arr[0];
+//
+//	for (n = 1;n < len;n++) {
+//		if (max < arr[n]) {
+//			max = arr[n];
+//		}
+//	}
+//	printf("max=%d\n", max);
+//	return 0;
+//}
+
+//³Ë·¨¿Ú¾÷
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	int n = 0;
+//	int sz = 0;
+//	for (i = 1;i < 10;i++) {
+//		for (n = 1;n <= i;n++) {
+//			sz = n * i;
+//			printf("%d¡Á%d=%d\t", n, i, sz);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
